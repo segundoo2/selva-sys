d9a67b534431baea8abf751d7b598b4e780eebae
Edilson Segundo <edilsonsilvaa13@gmail.com>
2025-02-22 15:43:36 -0300

    remove componente de rodapé e ajusta estilos do formulário de login para melhor responsividade

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,21 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
-    HeaderComponent,
-    FooterComponent
+    HeaderComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     HeaderComponent,
-    FooterComponent
   ]
 })
 export class SharedModule { }
