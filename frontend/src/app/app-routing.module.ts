9d6f492a6a6f3d94d51cac8f3aec3ea9943e91c1
Edilson Segundo <edilsonsilvaa13@gmail.com>
2025-03-17 18:03:34 -0300

    adiciona página inicial e configura rotas para redirecionamento

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormLoginComponent } from './modules/login/form-login/form-login.component';
+import { HomePageComponent } from './modules/home/home-page/home-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: FormLoginComponent },
+  { path: 'home', component: HomePageComponent },
+  { path: '**', redirectTo: '/login', pathMatch: 'full' },
 ];
 
 @NgModule({
